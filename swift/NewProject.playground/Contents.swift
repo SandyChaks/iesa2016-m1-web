8572b0b2903de310442d07d519fb5bf9fba32fc6
Mastere 1 IT <mastere1it@iMacdeAutres9.fro-206.froment.in.iesanetwork.com>
2016-06-15 14:25:54 +0200

    #2 Constantes et Variables

---
+++ swift/NewProject.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+// 02 Variables
+
+var myvariable = 42
+
+myvariable = 50
+
+var myOtherVariable:Double
+
+// 03 
+
+let implicitInteger = 70
+
+let ImplicitDouble = 70.0
+
+let explicitDouble :Double = 70
+
+
